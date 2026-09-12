a577cb415d37f14b4b8b88b64b9fc8e6a897acbf
gareytwin1 <gareytwin1@gmail.com>
2026-09-11 22:11:56 -0500

    add initial webpage

--- app/main.py
+++ app/main.py
@@ -1,15 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
+
+from simulator import PlantSimulator
 
 app = Flask(
-        name,
-        template_folder="../templates",
-        static_folder = "../static"
-      )
+    __name__,
+    template_folder="../templates",
+    static_folder="../static",
+)
+
+simulator = PlantSimulator()
+
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", state=simulator.get_state())
 
-if name == "main":
+
+@app.route("/start", methods=["POST"])
+def start():
+    simulator.start()
+    return redirect(url_for("home"))
+
+
+@app.route("/stop", methods=["POST"])
+def stop():
+    simulator.stop()
+    return redirect(url_for("home"))
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-
---
+++ app/simulator.py
@@ -0,0 +1,22 @@
+class PlantSimulator:
+    def __init__(self):
+        self.running = False
+        self.pressure = 100.0
+        self.temperature = 75.0
+        self.flow = 0.0
+
+    def start(self):
+        self.running = True
+        self.flow = 50.0
+
+    def stop(self):
+        self.running = False
+        self.flow = 0.0
+
+    def get_state(self):
+        return {
+            "running": self.running,
+            "pressure": self.pressure,
+            "temperature": self.temperature,
+            "flow": self.flow,
+        }
